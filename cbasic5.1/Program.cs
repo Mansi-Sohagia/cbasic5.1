94ecb15902f47dc8ec836872d8b312301995e080
TechJerryyy <msohagia@aakashinformatics.com>
2023-02-15 16:49:22 +0530

    Add project files.

---
+++ cbasic5.1/Program.cs
@@ -0,0 +1,28 @@
+﻿/*1- Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder. Display the count on the console.
+*/
+
+using System;
+
+namespace excercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, d;
+            Console.WriteLine("enter numbers n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter value of divider");
+            d = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("numbers " + n + " divided by " + d + " is:- ");
+
+            for (int i = 0; i <= n; i++)
+            {
+                if (i % d == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
